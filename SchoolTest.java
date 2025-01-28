d846cb659e7073594582cd7d8cb0351ae50f345c
chandrika kolluri <chandu052001@gmail.com>
2025-01-27 17:21:25 -0800

    added some method tests

--- SchoolTest.java
+++ SchoolTest.java
@@ -3,5 +3,7 @@ public class SchoolTest {
     public static void main(String[] args) {
         School mySchoo = new School("SOME", 30, 78);
         mySchoo.newDetails();
+        // myschoo.details; can't access because no object created for this
     }
+
 }
