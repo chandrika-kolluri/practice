edb898ce13b21d577dbb3540eeba24b037667451
chandrika kolluri <chandu052001@gmail.com>
2025-01-27 18:18:08 -0800

    added few more arrays and loops

--- Array.java
+++ Array.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Array {
 
     public static void main(String[] args) {
@@ -50,6 +52,10 @@ public class Array {
         for(int num : arr2) {
             System.out.println(num);
         }
+        System.out.println("____________________");
+        // to write all arrays in line we have to import array
+        // to convert array to string
+        System.out.println(Arrays.toString(arr2));
 
     }
     
