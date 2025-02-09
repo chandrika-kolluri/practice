ca45b8171e0691749891477e76fd9af3ef7d94fe
chandrika kolluri <chandu052001@gmail.com>
2025-02-09 14:23:59 -0800

    created package

---
+++ mypackage/MyClass.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+public class MyClass {
+    String name = "Lion";
+    String breed = "xyz";
+    int num = 80;
+
+    public void display() {
+        System.out.println("Name of the Animal is " + name + "\nBreed is " + breed + "\nTotal Number of Animals is " + num);
+    }
+}
